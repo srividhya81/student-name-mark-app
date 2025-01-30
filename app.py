57e47eb0124886f682eac72d23db9d5b6c59bf5e
srividhya81 <23ds1000022@ds.study.iitm.ac.in>
2025-01-30 20:01:35 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all origins
+
+# Load the student data from the JSON file
+with open('students.json') as f:
+    student_data = json.load(f)
+
+# Create a dictionary for fast lookup by name
+student_dict = {student['name']: student['marks'] for student in student_data['students']}
+
+@app.route('/api', methods=['GET'])
+def get_marks():
+    # Retrieve the 'name' query parameters from the request
+    names = request.args.getlist('name')
+    
+    # Look up the marks for each name
+    marks = [student_dict.get(name, "Not Found") for name in names]
+    
+    # Return the results in a JSON response
+    return jsonify({"marks": marks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
